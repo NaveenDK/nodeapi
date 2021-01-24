52a7952a3668786af2a57484101b2fed04aa53ed
NaveenDK <naveenkolambage@gmail.com>
2021-01-25 01:56:42 +1300

    added middleware

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 
-app.get("/", (req,res)=>{
-    res.send("Hello world from nodejs");
-})
+//bring in routes
+const {getPosts }= require('./routes/post');
+
+const myOwnMiddleware = (req,res,next)=>{
+    console.log("middleware applied!!!");
+    next();
+}
+
+//middleware
+app.use(morgan("dev"));
+app.use(myOwnMiddleware)
+
+app.get("/",getPosts);
 
 const port = 8080;
 
