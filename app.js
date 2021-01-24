edd4d4bf8989bf4628db6962b93c81cf4a5b611a
NaveenDK <naveenkolambage@gmail.com>
2021-01-25 01:07:42 +1300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Hello world from nodejs");
+})
+
+const port = 8080;
+
+app.listen(port,()=>{
+    console.log(`A nodejs api is listening on port:${port}`)
+});
