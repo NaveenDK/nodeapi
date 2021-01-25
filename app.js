f72068de56bd83ba2c2e6ffd537f9e39de8ceb69
NaveenDK <naveenkolambage@gmail.com>
2021-01-25 17:18:38 +1300

    added mongodb,env

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const morgan = require("morgan");
+// import mongoose
+const mongoose = require('mongoose');
+// load env variables
+const dotenv = require('dotenv');
 
 //bring in routes
 const postRoutes = require('./routes/post');
@@ -20,4 +24,18 @@ const port = 8080;
 
 app.listen(port,()=>{
     console.log(`A nodejs api is listening on port:${port}`)
+});
+
+
+dotenv.config()
+ 
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+ 
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
 });
